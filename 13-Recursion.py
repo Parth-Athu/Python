d6ee94ef028ca15a4721475c0f5c5bd203e73d74
Parth Athu <parthathu10@gmail.com>
2025-05-21 17:40:27 +0530

    Add Recursion  file

---
+++ 13-Recursion.py
@@ -0,0 +1,8 @@
+# def countdown(n):
+#     if n == 0:                    # Base case: stop when n reaches 0
+#         print("Blast off!")
+#     else:
+#         print(n)
+#         countdown(n - 1)          # Recursive call with smaller input
+
+# countdown(5)
